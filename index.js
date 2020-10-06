3e2d3e8888bd523115eb7212fd5a613e15cf782a
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-10-06 13:13:34 -0400

    complete refactoring, i.e. reset button pressed

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 const express = require("express")
-const postsRouter = require("./posts/posts-router")
+// const postsRouter = require("./posts/posts-router")
+// const welcomeRouter = require("./welcome/welcome-router")
 
 const server = express()
 const port = 5000
 
 server.use(express.json())
-server.use(postsRouter)
-
-server.get("/api/posts", (req, res) => {
-    posts.find()
-        .then((posts) => {
-            res.status(200).json(posts)
-        })
-        .catch((error) => {
-            console.log(error)
-            res.status(500).json({
-                error: "The posts information could not be retrieved."
-            })
-        })
-})
-
-/* When the client makes a GET request to /api/posts:
-
-If there's an error in retrieving the posts from the database:
-cancel the request.
-respond with HTTP status code 500.
-return the following JSON object: { error: "The posts information could not be retrieved." }.
-*/
+// server.use(posts-router)
+// server.use(welcomeRouter)
 
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
+	console.log(`Server running at http://localhost:${port}`)
 })
